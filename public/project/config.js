a64a288d392c1d3129194a7a026b50d1e1f48fcd
sravani <beeram.s@husky.neu.edu>
2016-03-03 17:08:53 -0500

    Project - Basic version

--- public/project/config.js
+++ public/project/config.js
@@ -1,7 +1,7 @@
 (function() {
     "use strict";
     angular
-        .module("FormBuilderApp")
+        .module("PageEditorApp")
         .config(Configure);
 
 
@@ -12,27 +12,14 @@
                 templateUrl: "views/home/home.view.html"
             })
 
-            .when("/admin", {
-                templateUrl: "views/admin/admin.view.html"
+            .when("/signup", {
+                templateUrl: "views/users/signup.view.html",
+                controller:"SignUpController"
             })
 
             .when("/profile", {
                 templateUrl: "views/users/profile.view.html",
                 controller:"ProfileController"
-            })
-
-            .when("/form", {
-                templateUrl: "views/forms/forms.view.html",
-                controller:"FormController"
-            })
-
-            .when("/fields", {
-                templateUrl: "views/forms/fields.view.html",
-            })
-
-            .when("/register", {
-                templateUrl: "views/users/signup.view.html",
-                controller:"RegisterController"
             })
 
             .when("/login", {
---
+++ public/project/views/users/login.controller.js
@@ -0,0 +1,24 @@
+(function() {
+    "use strict";
+    angular.module("PageEditorApp")
+           .controller("LoginController",LoginController);
+
+    function LoginController($scope, UserService,$rootScope,$location) {
+        $scope.login = login;
+        $scope.message = null;
+
+        function login(user) {
+            UserService.findUserByCredentials(user.username, user.password,render);
+
+            function render(user) {
+                if(user){
+                    $rootScope.currentUser = user;
+                    UserService.setCurrentUser(user);
+                    $location.url("/profile");
+                } else {
+                $scope.message = "Username or password doesnot match";
+                }
+            }
+        }
+    }
+})();
