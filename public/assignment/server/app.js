d3eb507c88047d31352823398b4b00ea54f41c36
sravani <beeram.s@husky.neu.edu>
2016-03-17 21:17:22 -0400

    Assignment 3 - Forms

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,9 +1,9 @@
 module.exports = function(app){
     var userModel = require("./models/user.model.js")(app);
     var formModel = require("./models/form.model.js")(app);
-    var fieldModel = require("./models/field.model.js")(formModel);
+  //  var fieldModel = require("./models/field.model.js")(formModel);
 
     var userService = require("./services/user.service.server.js")(app,userModel);
     var formService = require("./services/form.service.server.js")(app,formModel);
-    var fieldService = require("./services/field.service.server.js")(app,formModel,fieldModel);
+  //  var fieldService = require("./services/field.service.server.js")(app,formModel,fieldModel);
 };
